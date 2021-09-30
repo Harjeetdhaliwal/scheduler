87e3f46e67076c64b0a98a265ce21597498a82de
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-09-29 22:07:04 -0400

    Create form component and add stories

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+export default function Form(props) {
+
+  const {onSave , onCancel} = props;
+
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+
+          /*
+            This must be a controlled component
+          */
+          />
+        </form>
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={onCancel}>Cancel</Button>
+          <Button confirm onClick={onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
