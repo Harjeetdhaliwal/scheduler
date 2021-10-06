02f51286aad54c7a20f2ad3c38811f550a623614
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-10-05 20:11:53 -0400

    Install prop-types dependency and test type of props

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from "prop-types";
 
 export default function InterviewerList(props) {
-
-  const allInterviewers = props.interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} {...interviewer} selected={props.value === interviewer.id} setInterviewer={event => props.onChange(interviewer.id)} />)
+  const allInterviewers = props.interviewers.map((interviewer) => (
+    <InterviewerListItem
+      key={interviewer.id}
+      {...interviewer}
+      selected={props.value === interviewer.id}
+      setInterviewer={(event) => props.onChange(interviewer.id)}
+    />
+  ));
   return (
-   <section className="interviewers">
-     <h4 className="interviewers__header text--light">Interviewer</h4>
-     <ul className="interviewers__list">{allInterviewers}</ul>
-   </section>
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{allInterviewers}</ul>
+    </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
